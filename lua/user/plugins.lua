cfcfd82e2b1ecbaa69439f5c33886084a29e823a
Michael Ramos <michaeltramos@protonmail.com>
2022-07-08 11:22:32 -0400

    indent plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -69,7 +69,11 @@ return packer.startup(function(use)
   use {
     "beauwilliams/focus.nvim", config = function() require("focus").setup() end
   }
-
+	use {
+		'nvim-treesitter/nvim-treesitter',
+    --run = ':TSUpdate'
+  }
+  use "lukas-reineke/indent-blankline.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
