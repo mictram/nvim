316d1b0191fa2c0941215154ee4cb8b84308df7e
Michael Ramos <michaeltramos@protonmail.com>
2022-07-16 08:23:29 -0400

    add trouble plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -92,6 +92,17 @@ return packer.startup(function(use)
     "hrsh7th/cmp-vsnip",
     "hrsh7th/vim-vsnip",
   }
+	use {
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
+	}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
