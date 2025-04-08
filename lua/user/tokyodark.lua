f4851d89e105c2623c6eef35b5559eea7afe4b5b
mictram <michaeltramos@protonmail.com>
2025-04-08 11:02:01 -0400

    saving work:

--- lua/user/tokyodark.lua
+++ lua/user/tokyodark.lua
@@ -3,8 +3,3 @@ vim.g.tokyodark_transparent_background = true
 vim.g.tokyodark_enable_italic_comment = true
 vim.g.tokyodark_enable_italic = true
 vim.g.tokyodark_color_gamma = "1.0"
-
-local ok, _ = pcall(vim.cmd, 'colorscheme tokyodark')
-if not ok then
-  vim.cmd 'colorscheme default' -- if the above fails, then use default
-end
